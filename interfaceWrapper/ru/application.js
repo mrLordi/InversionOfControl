018e9d39c3a9f2fc2b0bbe353c75d480ce1ea077
mrLordi <denko1997zloy@yandex.ua>
2016-05-25 22:09:47 +0300

    all

--- interfaceWrapper/ru/application.js
+++ interfaceWrapper/ru/application.js
@@ -1,16 +1,18 @@
-/*// Вывод из глобального контекста модуля
-console.log('From application global context');
-
-// Объявляем функцию для события таймера
-function timerEvent() {
-  console.log('From application timer event');
-}
-
-// Устанавливаем функцию на таймер
-setTimeout(timerEvent, 1000);*/
-
 var fileName = './README.md';
+var countIteration = 10;
 console.log('Application going to read ' + fileName);
-fs.readFile(fileName, function(err, src) {
-  console.log('File ' + fileName + ' size ' + src.length);
-});
+setTimeout(function f(){
+	countIteration--;
+
+	fs.readFile(fileName, function(err, src) {
+		console.log('File ' + fileName + ' size ' + src.length);
+	});
+
+	if (countIteration>0) {
+		setTimeout(f, 2500);
+	}
+}, 1000);
+
+setInterval(function(){
+	printInformation();
+}, 3000);
--- interfaceWrapper/ru/framework.js
+++ interfaceWrapper/ru/framework.js
@@ -8,51 +8,69 @@ var context = {
   module: {},
   console: console,
   // Помещаем ссылку на fs API в песочницу
-  fs: cloneInterface(fs),
-  // Оборачиваем функцию setTimeout в песочнице
-  /*setTimeout: function(callback, timeout) {
-    // Добавляем поведение при вызове setTimeout
-    console.log(
-      'Call: setTimeout, ' +
-      'callback function: ' + callback.name + ', ' +
-      'timeout: ' + timeout
-    );
-    setTimeout(function() {
-      // Добавляем поведение при срабатывании таймера
-      console.log('Event: setTimeout, before callback');
-      // Вызываем функцию пользователя на событии таймера
-      callback();
-      console.log('Event: setTimeout, after callback');
-    }, timeout);
-  }*/
+  fs: wrapAll(fs),
+  setTimeout: setTimeout,
+  setInterval: setInterval,
+  printInformation: printInformation
 };
 
+var sandbox = vm.createContext(context);
+var countCallFunction = 0;
+var coutCallBack = 0;
+var avgTime = 0;
+
 function cloneInterface(anInterface) {
-  var clone = {};
-  for (var key in anInterface) {
-    clone[key] = anInterface[key];
-  }
-  return clone;
+	var clone = {};
+    for (var key in anInterface) {
+    	clone[key] = anInterface[key];
+    }
+    return clone;
 }
 
 function wrapFunction(fnName, fn) {
-  return function wrapper() {
-    var args = [];
-    Array.prototype.push.apply(args, arguments);
-    console.log('Call: ' + fnName);
-    console.dir(args);
+    return function wrapper() {
+      var args = [];
+      countCallFunction++;
+      Array.prototype.push.apply(args, arguments);
 
-    if(typeof args[args.length - 1] === 'function' ) {
-    	args[args.length - 1] = wrapFunction(fnName + ' : callback', args[args.length - 1]);
+      console.log('Call: ' + fnName);
+      //console.dir(args);
+
+      if(typeof args[args.length - 1] === 'function' ) {
+         args[args.length - 1] = wrapFunction(fnName + ' : callback',
+         	args[args.length - 1]);
+         coutCallBack++;
+      }
+
+      var start = process.hrtime();
+      var result = fn.apply(undefined, args);
+      var end = process.hrtime(start);
+      var timeS = end[0] + (end[1] / 1000000000);
+      avgTime = ((countCallFunction - 1)*avgTime + timeS)/countCallFunction;
+      return result;
     }
-
-    return fn.apply(undefined, args);
   }
+
+function wrapAll(api) {
+	var wraperedApi = cloneInterface(api);
+	for(var key in wraperedApi) {
+	   if(typeof wraperedApi[key] === 'function') {
+	       wraperedApi[key] = wrapFunction(key, wraperedApi[key]);
+	   }
+	}
+	return wraperedApi;
+}
+
+function printInformation(){
+	console.log('--------Iformation---------');
+	console.log('Calls: ' + countCallFunction);
+	console.log('CallBack: ' + coutCallBack);
+    console.log('Time: ' + avgTime + ' s');
+    console.log('---------------------------');
 }
 
 // Преобразовываем хеш в контекст
 context.global = context;
-var sandbox = vm.createContext(context);
 
 // Читаем исходный код приложения из файла
 var fileName = './application.js';
@@ -61,3 +79,4 @@ fs.readFile(fileName, function(err, src) {
   var script = vm.createScript(src, fileName);
   script.runInNewContext(sandbox);
 });
+
